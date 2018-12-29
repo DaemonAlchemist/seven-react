28ded38b66830bd34da57df2a520fd50722da8d0
Andrew Wittrock <andy@wittrock.us>
2018-12-29 13:59:45 -0600

    Fix bid display on overview page

--- src/components/Overview/Overview.container.tsx
+++ src/components/Overview/Overview.container.tsx
@@ -1,4 +1,3 @@
-import {_, map, partitionOn} from 'atp-pointfree';
 import {push} from 'connected-react-router';
 import {connect} from 'react-redux';
 import { getBids, getPlayers, getRounds } from '../../util/Seven.redux';
@@ -8,12 +7,9 @@ import { IOverviewDispatchProps, IOverviewStateProps } from './Overview.types';
 
 export const Overview = connect<IOverviewStateProps, IOverviewDispatchProps>(
     (state:IGameContainer) => ({
-        getBid: (() => {
-            const bids = _(map(partitionOn("playerId")), partitionOn("roundId"))(getBids(state) || []) || {};
-            return (roundId:number, playerId:number):IBid => bids[roundId] && bids[roundId][playerId]
-                ? bids[roundId][playerId]
-                : {roundId, playerId};
-        })(),
+        getBid: (roundId:number, playerId:number) => getBids(state)
+            .filter((bid:IBid) => bid.roundId === roundId && bid.playerId === playerId)[0]
+            || {roundId, playerId},
         players: getPlayers(state),
         rounds: getRounds(state),
     }),
