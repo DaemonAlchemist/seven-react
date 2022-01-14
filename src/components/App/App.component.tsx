6993ea8fda6a70e3b3d5595a44e7d07661d2d5f3
Andrew Wittrock <andy@wittrock.us>
2022-01-14 10:36:28 -0600

    Fix top header collasping issue

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,4 +1,5 @@
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
@@ -6,14 +7,15 @@ import { NewGame } from '../NewGame';
 import { Overview } from '../Overview';
 import { SetBids } from '../SetBids';
 import './App.scss';
-import 'antd/dist/antd.css';
 
 export const App = () => 
     <Layout className="app">
       <Layout.Header>
-        <Menu theme="dark" mode="horizontal">
-          <Menu.Item>Andy's Awesome Seven Scoring App</Menu.Item>
-        </Menu>
+        <ul className="ant-menu ant-menu-horizontal ant-menu-dark">
+            <li className="ant-menu-item">
+                Andy's Awesome Seven Scoring App
+            </li>
+        </ul>
       </Layout.Header>
       <Layout.Content>
           <BrowserRouter>
